48465f6d4b635e7e24a47f0ac824a0a721da6784
edenlisk <nkumbuyenock@gmail.com>
2024-05-24 15:04:51 +0200

    Auto generated tests

---
+++ src/test/java/me/edenlisk/springboottodo/SpringBootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.edenlisk.springboottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
