b0ef1037cf694d5ccf3ee4e4f8e7ffa86999e02f
edenlisk <nkumbuyenock@gmail.com>
2024-05-26 23:23:27 +0200

    Use correct table name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS todos (
+CREATE TABLE IF NOT EXISTS todo (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    userId VARCHAR(50) NOT NULL,
+    user_id VARCHAR(50) NOT NULL,
     description VARCHAR(50) NOT NULL,
     category VARCHAR(30) NOT NULL,
-    isCompleted BOOLEAN NOT NULL,
-    targetDate DATE NOT NULL,
+    is_completed BOOLEAN NOT NULL,
+    target_date DATE NOT NULL,
     PRIMARY KEY (id)
 );
-
-INSERT INTO todos(userId, description, category, isCompleted, targetDate)
+--
+INSERT INTO todo(user_id, description, category, is_completed, target_date)
 VALUES ('user1', 'Learn to Dance', 'Hobby', false, '2021-12-31'),
        ('user1', 'Do Laundry', 'Housework', false, '2021-12-31'),
        ('user1', 'Buy Groceries', 'Shopping', false, '2021-12-31'),
